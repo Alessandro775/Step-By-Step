cc02b5bff080e8b7a3be035f9ac36147fdb6a617
NicolasBello03 <n.bello3@studenti.uniba.it>
2025-06-09 15:54:27 +0200

    pagina di registrazione

--- src/utils/RoutePath.jsx
+++ src/utils/RoutePath.jsx
@@ -2,6 +2,7 @@ import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import ProfilePage from "../pages/ProfilePage";
 import PracticePage from "../pages/PracticePage";
+import RegisterPage from "../pages/RegisterPage";
 import { Route, Routes } from "react-router-dom";
 
 function RoutesPath() {
@@ -11,6 +12,7 @@ function RoutesPath() {
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage/>}/>
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/practice" element={<PracticePage />} />
         <Route path="*" element={<HomePage />} /> {/* Questa rotta gestisce i percorsi non trovati */}
